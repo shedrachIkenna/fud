521c2f7713bade2abe27b24e2f12ec5947b53393
shedrachIkenna <shedrach686@gmail.com>
2022-11-20 15:26:33 +0100

    Finished banner component

--- fud/components/Banner.js
+++ fud/components/Banner.js
@@ -1,10 +1,31 @@
-
+import Image from 'next/image'
+import BannerImage from '../static/banner.png'
 const Banner = () => {
     const styles={
-        bannerText:'max-w-xl, text-[6rem] font-mediumSerif'
+        wrapper:'h-max-[10rem] flex items-center justify-center bg-[#FCC017] border-y border-black',
+        content:'max-w-7xl flex-1 flex items-center justify-between border border-black',
+        bannerWrapper:'space-y-5 px-10 flex-[3]',
+        bannerTitle:'max-w-xl, text-[6rem] font-mediumSerif',
+        bannerText:'text-2xl',
+        accentedButton:'bg-black text-white py-2 px-4 rounded-full',
+        bannerImageStyle:'object-contain',
     }
     return (
-        <h1 className={styles.bannerText}>Stay Curious.</h1>
+        <div className={styles.wrapper}>
+            <div className={styles.content}>
+                <div className={styles.bannerWrapper}>
+                    <h1 className={styles.bannerTitle}>Stay Curious.</h1>
+                    <h3 className={styles.bannerText}>Discover stories, thinking, and expertise from writers on any topic</h3>
+                    <button className={styles.accentedButton}>Start Reading</button>
+                </div>
+                <Image
+                    className={styles.bannerImageStyle}
+                    src={BannerImage}
+                    width={500}
+                    height={400}
+                />
+            </div>
+        </div>
     )
 }
 
