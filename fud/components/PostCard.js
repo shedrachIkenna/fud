305aea2eedb55400c045945537992e89f2babfd7
shedrachIkenna <shedrach686@gmail.com>
2022-11-24 01:59:12 +0100

    Finished postCard component styling

--- fud/components/PostCard.js
+++ fud/components/PostCard.js
@@ -1,26 +1,53 @@
 import Image from 'next/image'
 import Logo from '../static/logo.png'
+import { FiBookmark } from 'react-icons/fi'
 
 const styles = {
-    postDetails: '',
+    wrapper: 'flex max-w-[46rem] h-[10rem] items-center gap-[1rem] cursor-pointer',
+    postDetails: 'flex-[2.5] flex flex-col',
     authorContainer: 'flex gap-[.4rem]',
     authorImageContainer: 'grid place-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem] border border-black',
     authorImage: 'object-cover',
     authorName:'font-semibold',
+    postCardTitle : 'font-bold text-2xl',
+    briefing: 'text-[#787878]',
+    produvtivityText: 'bg-[#F2F3F2] p-1 rounded-full',
+    articleDetails: 'my-2 text-[.8rem]',
+    detailsContainer: 'flex items-center justify-between text-[#787878]',
+    bookmarkContainer: 'cursor-pointer'
 }
 
 const PostCard = () => {
     return (
-        <div className={styles.authorContainer}>
-            <div className={styles.authorImageContainer}>
-                <Image
+        <div className={styles.wrapper}>
+            <div className={styles.postDetails}>
+                <div className={styles.authorContainer}>
+                    <div className={styles.authorImageContainer}>
+                        <Image
+                            src={Logo}
+                            className={styles.authorImage}
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                    <div className={styles.authorName}>Shedrach Ikenna</div>
+                </div>
+                <h1 className={styles.postCardTitle}>7 free tools that will make you more productive in 2022</h1>
+                <div className={styles.briefing}>Productivity is a skill that can be learned</div>
+                <div className={styles.detailsContainer}>
+                    <span className={styles.articleDetails}>June 15 • 5 min read • <span className={styles.produvtivityText}>productivity</span></span>
+                    <span className={styles.bookmarkContainer}>
+                        <FiBookmark className='h-5 w-5'/>
+                    </span>
+                </div>
+            </div>
+            <div className={styles.thumbnailContainer}>
+                <Image 
+                    height={100}
+                    width={100}
                     src={Logo}
-                    className={styles.authorImage}
-                    width={40}
-                    height={40}
                 />
             </div>
-            <div className={styles.authorName}>Shedrach Ikenna</div>
         </div>
     )
 }
--- fud/pages/index.js
+++ fud/pages/index.js
@@ -2,12 +2,29 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import PostCard from '../components/PostCard'
 
+const styles={
+  wrapper: 'mx-auto',
+  main: 'flex justify-center',
+  container: 'max-w-7xl flex-1 border border-black',
+  postsList: 'flex flex-col gap-3 md:g-6 p-2 md:p-6 sm:grid-cols-2 lg:grid-cols-3'
+}
+
 export default function Home() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
       <Banner />
-      <PostCard />
-    </>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.postsList}>
+            <PostCard />
+            <PostCard />
+            <PostCard />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+
+
